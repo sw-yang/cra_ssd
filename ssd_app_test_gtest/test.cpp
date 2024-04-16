f94ecb2ddaf27328e5e08baeafa8620022b54510
sw-yang <sw3.yang@samsung.com>
2024-04-16 11:47:37 +0900

    [Add] SSD Test APP Init Commit
    SSD mock class 추가 및 빌드 테스트 완료.

--- ssd_app_test_gtest/test.cpp
+++ ssd_app_test_gtest/test.cpp
@@ -1,6 +1,18 @@
 #include "pch.h"
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+class MockSSDApp {
+public:
+	MOCK_METHOD(void, write, (int addr, int data), ());
+	MOCK_METHOD(void, read, (int addr), ());
+};
 
 TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+	MockSSDApp app;
+
+	EXPECT_CALL(app, write(0, 0x12345678))
+		.Times(1);
+
+	app.write(0, 0x12345678);
 }
