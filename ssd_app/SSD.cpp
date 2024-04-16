45d33a40b0239c3e87d1e77880884cfaf7727151
hjjun <hjjun@mail.com>
2024-04-16 14:50:56 +0900

    Refactor1 - Read default data

--- ssd_app/SSD.cpp
+++ ssd_app/SSD.cpp
@@ -19,16 +19,41 @@ void SSD::Read(uint32_t address)
 	}
 
 	std::ifstream in(nand_file_, std::ios::binary);
-	if (!in.is_open()) {
+	if (!in.is_open())
+	{
 		throw std::exception("Fail to open nand file");
 	}
 
 	std::ofstream out(result_file_, std::ios::trunc);
-	if (!out.is_open()) {
+	if (!out.is_open())
+	{
 		throw std::exception("Fail to open result file");
 	}
 
-	out << "0x00000000";
+	// check file size
+	in.seekg(0, std::ios::end);
+	std::streampos fileSize = in.tellg();
+
+	if (fileSize != 400)
+	{
+		// Invalid nand file data
+		for (int i = 0; i < 100; ++i)
+		{
+			nand_[i] = 0;
+		}
+	}
+
+	std::ostringstream ss;
+	ss << nand_[address];
+	int zero_fills = 8 - ss.str().length();
+
+	std::string result = "0x";
+	for (int i = 0; i < zero_fills; ++i) {
+		result += "0";
+	}
+	result += ss.str();
+
+	out << result;
 }
 
 void SSD::Write(uint32_t address, uint32_t value)
