638ad34511c262494a9f0dd83e9efe4f3be6809d
whanseok <whanseok.kim@samsung.com>
2024-04-17 14:29:48 +0900

    [bugFix] Read Command 인자 개수 오류 수정

--- ssd_app/CMD.cpp
+++ ssd_app/CMD.cpp
@@ -43,10 +43,20 @@ unsigned int Command::hexStringToInt(const string& hexStr) {
 
 bool Command::isInvalidCommand()
 {
-	if (args.size() != 3) return true;
-	if (!(args[0] == "R" || args[0] == "W")) return true;
-	if (!isNumber(args[1])) return true;
-	if (stoi(args[1]) < 0 || stoi(args[1]) > 99) return true;
-	if (!isValidHex(args[2])) return true;
+	if (args.size() == 2 && args[0] == "R")
+	{
+		if (!isNumber(args[1])) return true;
+		if (stoi(args[1]) < 0 || stoi(args[1]) > 99) return true;
+	}
+	else if (args.size() == 3 && args[0] == "W")
+	{
+		if (!isNumber(args[1])) return true;
+		if (stoi(args[1]) < 0 || stoi(args[1]) > 99) return true;
+		if (!isValidHex(args[2])) return true;
+	}
+	else
+	{
+		return true;
+	}
 	return false;
 }
