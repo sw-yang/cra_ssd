52673d855cfbf1c39703e02305cbf6763e598595
whanseok <whanseok.kim@samsung.com>
2024-04-17 13:49:20 +0900

    [ADD]main file 누락으로 재커밋

--- ssd_app/main.cpp
+++ ssd_app/main.cpp
@@ -3,23 +3,23 @@
 
 int main(int argc, char* argv[]) {
 
-    if (argc <= 1) return 0;
-     vector<string> args;
-    for (int i = 1; i < argc; ++i) 
-    {  
-        args.push_back(argv[i]);
-    }
+	if (argc <= 1) return 0;
+	vector<string> args;
+	for (int i = 1; i < argc; ++i)
+	{
+		args.push_back(argv[i]);
+	}
 
-    SSD ssd;
-    Command cmd(args);
-    try
-    {
-        ssd.Run(cmd);
-    }
-    catch (std::exception& e)
-    {
-        std::cout << e.what() << std::endl;
-    }
-    
-    return 0;
+	SSD ssd;
+	Command cmd(args);
+	try
+	{
+		ssd.Run(cmd);
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	return 0;
 }
