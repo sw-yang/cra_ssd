69a5d047384e4da95739d8e806426fc2e85fcb6a
lee9142669 <lee9142669@naver.com>
2024-04-18 13:43:47 +0900

    [Refactoring]Coverter와 InvalidChecker 분리
    (cherry picked from commit 626a5104bbd31f0c3fe7c217c0d6e39816a93649)
    (cherry picked from commit 8002bb74834ad129f78ac38904fe08566357e7b3)

--- ssd_app_test/TestShell.h
+++ ssd_app_test/TestShell.h
@@ -2,6 +2,8 @@
 #include "ISSDApp.h"
 #include <string>
 #include <vector>
+#include "Test_Const.h"
+#include "Converter.h"
 
 enum TestShellCMD
 {
@@ -30,22 +32,10 @@ public:
     void ScriptRun(const char* script_path);
     void set_ssd_app(ISSDApp* app);
     ISSDApp* get_ssd_app(void) { return ssd_app; } //to be deleted
-    std::vector<int> addr_arr;//to be deleted
-    std::vector<int> data_arr;//to be deleted
+    std::vector<uint32_t> addr_arr;//to be deleted
+    std::vector<uint32_t> data_arr;//to be deleted
 private:
-    bool Input(void);
-    bool ConvertAddrToInt(const std::string& str_addr);
-    bool ConvertEraseEndAddrToInt(const std::string& str_endaddr);
-    bool IsInvalidAddrFormat(const std::string& str_addr);
-    bool IsInvalidAddrRange(void);
-
-    bool ConvertDataToInt(const std::string& str_data);
-    bool ConvertEraseSizeToInt(const std::string& str_data);
-    bool IsInvalidDataFormat(const std::string& str_data);
-    bool IsInvalidEraseSizeFormat(const std::string& str_data);
-
-    bool IsHexNum(const std::string& str);
-    bool IsDecNum(const std::string& str);
+    bool Input(void);;
 
     uint32_t Read(const uint32_t addr);
     std::vector<std::uint32_t> FullRead(void);
@@ -69,11 +59,7 @@ private:
     uint32_t addr;
     uint32_t data;
 
-    const uint32_t kAddrLen = 3;
-    const uint32_t kMinAddr = 0;
-    const uint32_t kMaxAddr = 99;
-    const uint32_t kDataLen = 10;
-
     PrintLevel cur_print_level = INFO;
     std::string user_input;
+    Converter converter;
 };
