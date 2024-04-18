6db4fe248180955c1d0f06b2f1335fc6092ef11a
leesyungki <165616302+leesyungki@users.noreply.github.com>
2024-04-18 11:37:54 +0900

    Erase shell 구현 완료 (#31)
    * [Add]Erase와 EraseRange구현 완료 (UT포함)
     - Mock에서만 검증완료. SSDEraseTest는 Disabled
    
    * [Add]Erase와 EraseRange구현 완료 (UT포함)
     - Mock에서만 검증완료. SSDEraseTest는 Disabled
    
    (cherry picked from commit d07047dbaf9424354a9754a24e35445f8ffd75e6)

--- ssd_app_test/ISSDApp.h
+++ ssd_app_test/ISSDApp.h
@@ -3,6 +3,7 @@
 class ISSDApp
 {
 public:
-    virtual void Write(const uint32_t addr, const uint32_t data) = 0;
     virtual uint32_t Read(const uint32_t addr) = 0;
+    virtual void Write(const uint32_t addr, const uint32_t data) = 0;
+    virtual void Erase(const uint32_t addr, const uint32_t size) = 0;
 };
